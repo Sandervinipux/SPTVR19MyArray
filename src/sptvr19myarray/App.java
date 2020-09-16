71619c66ac2f8e79e05d2791e00886f75f19289f
none <none@ee.ee>
2020-09-16 12:37:20 +0300

    proekt po massivam

--- src/sptvr19myarray/App.java
+++ src/sptvr19myarray/App.java
@@ -16,6 +16,10 @@ class App {
         System.out.println("Массивы !!!!!!!!!!!!______!!!!!!!!");
         Random random = new Random();
         int rnum = 0;
+        rnum = 0;
+        int min = 100;
+        int max = 0;
+        int sum = 0;
         //Создаём массив чётных целых чисел.
         int [] numbers = new int [20];
         for (int i = 0; i < numbers.length; i++) {
@@ -26,8 +30,13 @@ class App {
                     break;
                 }
             }
+            if (numbers[i] < max)max = numbers[i];
+            if (numbers[i] > max)max = numbers[i];
+            sum+= numbers[i];
             System.out.printf("%4d", numbers[i]);
         }
         System.out.println();
+        System.out.println("Среднее арифметическое без учёта минимального(+min+) и максимального(+max+)");
+        System.out.println((double)(sum-min-max)/(numbers.length - 2));
     }
 }
